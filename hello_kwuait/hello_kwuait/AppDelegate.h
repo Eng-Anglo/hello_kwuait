b21a1119ef45f1fbf8c4c76e3fb81081cd293625
Anglo Essam <angloes@moe.edu.kw>
2016-02-16 12:35:00 +0300

    This is test

---
+++ hello_kwuait/hello_kwuait/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello_kwuait
+//
+//  Created by Eng Anglo Essam Saber on 2/16/16.
+//  Copyright © 2016 hello_kwuait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
